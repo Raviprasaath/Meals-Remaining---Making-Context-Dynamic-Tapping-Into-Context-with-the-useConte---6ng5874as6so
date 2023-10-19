7cef9556f40577e593a7fd34c62f9fde40072144
Raviprasaath Ramesh <117162868+Raviprasaath@users.noreply.github.com>
2023-10-19 12:19:44 +0530

    Update MealsProvider.js

--- app/MealsProvider.js
+++ app/MealsProvider.js
@@ -10,13 +10,19 @@ const todaysMeals = [
 const MealsProvider = () => {
     const [meals, setMeals] = useState(todaysMeals);
 
-    const tickMeal = () => {
-
+    const tickMeal = (id) => {
+        setMeals(
+            meals.map((meal) => (
+                meal.id === id ? { ...meal, ticked: !meal.ticked } : meal
+            ))
+        );
     }
 
     return (
         <div>
-            
+            <MealsContext.Provider value={{ meals, tickMeal }}>
+                {children}
+            </MealsContext.Provider>
         </div>
     )
 };
